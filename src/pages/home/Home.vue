d87227c9b13ba0497ee06c3da2a1b1b1aac68de6
pengwenwe <1050210750@qq.com>
2017-11-03 21:11:51 +0800

    hotlist-pengwenwen

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,21 @@
 <template>
    <div>
-    <index-header />
-    <index-swiper />
+      <index-header />
+      <index-swiper />
+      <index-hotlist />
   </div>
 </template>
 
 <script>
  import header from "./Header.vue";
  import swiper from "./Swiper.vue";
+ import hotlist from "./Hotlist.vue";
+ 
   export default{
     components: {
       "index-header": header,
-      "index-swiper": swiper
+      "index-swiper": swiper,
+      "index-hotlist":hotlist
     }
   }
 </script>
