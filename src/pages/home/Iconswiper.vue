3d5f595a34e42836a7405dc483f425a0f1263405
tianyuanJ <29232751@qq.com>
2017-11-04 11:39:47 +0800

    jiang

--- src/pages/home/Iconswiper.vue
+++ src/pages/home/Iconswiper.vue
@@ -5,68 +5,12 @@
         <div class="category-container swipe">
             <div class="swipe-wrap">
                 <div class="swipe-item">
-                    <div class="category-item">
+                    <div class="category-item" v-for="list in pageone" :key="list.id">
                         <a>
                             <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png">
+                                <img :src="list.src">
                             </div>
-                            <div class="keywords">景点门票</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png">
-                            </div>
-                            <div class="keywords">动植物园</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/45/676b67d7078abc02.png">
-                            </div>
-                            <div class="keywords">故宫</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png">
-                            </div>
-                            <div class="keywords">一日游</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png">
-                            </div>
-                            <div class="keywords">必游榜单</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/6d/ee9eb965690ce402.png">
-                            </div>
-                            <div class="keywords">秋色美</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png">
-                            </div>
-                            <div class="keywords">游乐场</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png">
-                            </div>
-                            <div class="keywords">泡温泉</div>
+                            <div class="keywords">{{list.title}}</div>
                         </a>
                     </div>
                 </div>
@@ -76,69 +20,13 @@
     <swiper-slide>
         <div class="category-container swipe">
             <div class="swipe-wrap">
-                <div class="swipe-item">
-                    <div class="category-item">
+                <div class="swipe-item" >
+                     <div class="category-item" v-for="list in pagetwo" :key="list.id">
                         <a>
                             <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png">
+                                <img :src="list.src">
                             </div>
-                            <div class="keywords">城市观光</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/29/c16cd064d85d8502.png">
-                            </div>
-                            <div class="keywords">玻璃栈道</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/1c/a8a8d9023a814c02.png">
-                            </div>
-                            <div class="keywords">名胜古迹</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="https://img1.qunarzz.com/piao/fusion/1602/f2/71eb19f77e3f21f7.png">
-                            </div>
-                            <div class="keywords">周边游</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/cd/07f33cc087f18d02.png">
-                            </div>
-                            <div class="keywords">自然风光</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/ea/8cb1dce519375d02.png">
-                            </div>
-                            <div class="keywords">古北水镇</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1707/34/9285073fde1eb702.png">
-                            </div>
-                            <div class="keywords">景点讲解</div>
-                        </a>
-                    </div>
-                    <div class="category-item">
-                        <a>
-                            <div class="category-img-container">
-                                <img src="http://img1.qunarzz.com/piao/fusion/1611/be/a70e61e1f57ee302.png">
-                            </div>
-                            <div class="keywords">全部</div>
+                            <div class="keywords">{{list.title}}</div>
                         </a>
                     </div>
                 </div>
@@ -154,15 +42,81 @@
  import { swiper, swiperSlide } from 'vue-awesome-swiper';
   export default {
     data() {
-      return {
-          swiperOption: {
-          direction: 'horizontal',
-          autoHeight: true,
-          pagination:'.swiper-pagination',
-          bulletClass : 'my-bullet',
-          bulletActiveClass: 'bullet-active',
-          observeParents: true,
-          }
+    return {
+        swiperOption: {
+        direction: 'horizontal',
+        autoHeight: true,
+        pagination:'.swiper-pagination',
+        bulletClass : 'my-bullet',
+        bulletActiveClass: 'bullet-active',
+        observeParents: true,
+        },
+        pageone: [{
+            id: 1,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+            title: "景点门票"
+        },{
+            id: 2,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png",
+            title: "动植物园"
+        },{
+            id: 3,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/45/676b67d7078abc02.png",
+            title: "故宫"
+        },{
+            id: 4,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png",
+            title: "一日游"
+        },{
+            id: 5,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png",
+            title: "必游榜单"
+        },{
+            id: 6,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/6d/ee9eb965690ce402.png",
+            title: "秋色美"
+        },{
+            id: 7,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png",
+            title: "游乐场"
+        },{
+            id: 8,
+            src: "http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png",
+            title: "泡温泉"
+        }],
+        pagetwo: [{
+            id: 1,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png",
+            title: "城市观光"
+        },{
+            id: 2,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/29/c16cd064d85d8502.png",
+            title: "玻璃栈道"
+        },{
+            id: 3,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/1c/a8a8d9023a814c02.png",
+            title: "名胜古迹"
+        },{
+            id: 4,
+            src: "https://img1.qunarzz.com/piao/fusion/1602/f2/71eb19f77e3f21f7.png",
+            title: "周边游"
+        },{
+            id: 5,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/cd/07f33cc087f18d02.png",
+            title: "自然风光"
+        },{
+            id: 6,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/ea/8cb1dce519375d02.png",
+            title: "古北水镇"
+        },{
+            id: 7,
+            src: "http://img1.qunarzz.com/piao/fusion/1707/34/9285073fde1eb702.png",
+            title: "景点讲解"
+        },{
+            id: 8,
+            src: "http://img1.qunarzz.com/piao/fusion/1611/be/a70e61e1f57ee302.png",
+            title: "全部"
+        }]
         }
       },
       components: {
