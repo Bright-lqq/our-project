992e04801d82db7a5b26296509f89e214102d9b4
yumengjuan <1017633020@qq.com>
2017-11-06 20:51:48 +0800

    change-detail

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="detail-main">
+        <detail-Banner />
+        <detail-Base />
+    </div>
+</template>
+
+<script>
+    import Banner from "./Banner.vue";
+    import Base from "./Base.vue";
+
+    export default {
+        components: {
+            "detail-Banner":Banner,
+            "detail-Base":Base
+        }
+    }
+</script>
+
+<style scoped>
+   
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/Home/Home'
+import Detail from '@/pages/Detail/Detail'
+
 
 
 Vue.use(Router)
@@ -11,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+     {
+      path: '/detail',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
